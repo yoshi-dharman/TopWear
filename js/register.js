77a152c9e4ed4e174f143c3a34b2d0af93196ff3
Rendy Artha <rendyartha312@gmail.com>
2021-02-19 22:47:35 +0700

    logic login dan register

---
+++ js/register.js
@@ -0,0 +1,27 @@
+const login = async() => {
+  let emailInputRegister = document.getElementById("email-form").value;
+  let nameInputRegister = document.getElementById("name-form").value;
+  let passwordInputRegister = document.getElementById("password-form").value;
+
+  let userData = {
+    name: nameInputRegister,
+    email: emailInputRegister,
+    password: passwordInputRegister
+  }
+
+  if(nameInputRegister === "" || emailInputRegister === "" || passwordInputRegister === "") {
+    alert("Please input your data")
+  } else {
+    const api = "https://602f36924410730017c51afd.mockapi.io/user"
+    const response = await fetch(api, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    const result = await response.json()
+    alert("Registrasi berhasil, silahkan login")
+    window.location.href = "/index.html"
+  }
+}
